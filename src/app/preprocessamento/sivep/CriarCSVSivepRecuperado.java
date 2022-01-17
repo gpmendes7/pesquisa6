b63c558a51d66afefaec609758547430b3839c97
gabriel7mendes <andrewsseven@gmail.com>
2022-01-17 10:10:39 -0300

    Criar  csv sivep com registro de óbitos e de recuperados

--- src/app/preprocessamento/sivep/CriarCSVSivepRecuperado.java
+++ src/app/preprocessamento/sivep/CriarCSVSivepRecuperado.java
@@ -7,29 +7,28 @@ import java.util.List;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
+import app.util.StringUtil;
 import csv.PlanilhaMGCSV;
 import csv.PlanilhaMGCSVHandler;
 
-public class SepararRegistrosSivepComDataInternacaoEIdade {
+public class CriarCSVSivepRecuperado {
 	
 	public static void main(String[] args) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
-		List<PlanilhaMGCSV> registros = PlanilhaMGCSVHandler.carregarCSV("./arquivos/csv/SIVEP-MG(ComEvolucaoCasoObitoRecuperado).csv");
+		List<PlanilhaMGCSV> registros = PlanilhaMGCSVHandler.carregarCSV("./arquivos/csv/SIVEP-MG(ComDataInternacaoEIdade).csv");
 			
-		List<PlanilhaMGCSV> registrosSivepComDataInternacaoEIdade = new ArrayList<>();
+		List<PlanilhaMGCSV> registrosSivepRecuperado = new ArrayList<>();
 		
 		for (PlanilhaMGCSV registro : registros) {
-			String dataInternacao = registro.getDataInternacao();
-			String idade = registro.getIdade();
+			String evolucao = StringUtil.normalizarString(registro.getEvolucao());
 			
-			if((dataInternacao != null && !dataInternacao.equals(""))
-			    && (idade != null && !idade.equals(""))) {
-				registrosSivepComDataInternacaoEIdade.add(registro);
+			if(evolucao.equals("RECUPERADO")) {
+				registrosSivepRecuperado.add(registro);
 			}
 		}
 		
-		System.out.println("Total de registros SIVEP MG com data de internação e idade: " + registrosSivepComDataInternacaoEIdade.size());
+		System.out.println("Total de registros SIVEP MG recuperado: " + registrosSivepRecuperado.size());
 		
-		registrosSivepComDataInternacaoEIdade.add(0, new PlanilhaMGCSV("laboratoriox", "codigo", "dataNascimento", "municipio", 
+		registrosSivepRecuperado.add(0, new PlanilhaMGCSV("laboratoriox", "codigo", "dataNascimento", "municipio", 
 														         "urs", "sexo", "idade", "internacao", 
 														         "internacaoUti", "dataInternacao", "evolucao", "resultadoTeste", 
 														         "dataTeste", "comorbidade", "comorbidadeDetalhe", "raca", 
@@ -40,7 +39,8 @@ public class SepararRegistrosSivepComDataInternacaoEIdade {
 														         "bm2", "cm1", "cm2", "rm1", 
 														         "rm2", "qbm1", "qbm2", "qam1", "qam2"));
 		
-		PlanilhaMGCSVHandler.criarCSV("./arquivos/csv/SIVEP-MG(ComDataInternacaoEIdade).csv", registrosSivepComDataInternacaoEIdade);
+		PlanilhaMGCSVHandler.criarCSV("./arquivos/csv/SIVEP-MG(RECUPERADO).csv", registrosSivepRecuperado);
 	}
+
 
 }
