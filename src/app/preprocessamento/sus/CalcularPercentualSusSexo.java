652dd5cadf6f1693772f4bf66a383cfa2adfe380
gabriel7mendes <andrewsseven@gmail.com>
2023-02-11 15:45:26 -0300

    cálculo de percentual de homens e mulheres nos arquivos originais das bases esus e sivep

---
+++ src/app/preprocessamento/sus/CalcularPercentualSusSexo.java
@@ -0,0 +1,36 @@
+package app.preprocessamento.sus;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+import csv.PlanilhaMGCSV;
+import csv.PlanilhaMGCSVHandler;
+
+public class CalcularPercentualSusSexo {
+	
+	public static void main(String[] args) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+		List<PlanilhaMGCSV> registros = PlanilhaMGCSVHandler.carregarCSV("./arquivos/csv/sus/ESUS-MG.csv");
+		
+	    System.out.println("registros.size(): " + registros.size());
+	    
+	    int qtdHomens = 0;
+	    int qtdMulheres = 0;
+	    
+	    for (PlanilhaMGCSV registro : registros) {
+			if(registro.getSexo().equals("MASCULINO")) 
+				qtdHomens++;
+			else if(registro.getSexo().equals("FEMININO")) 
+				qtdMulheres++;
+		}
+	    
+	    float percentualHomens = ((float) qtdHomens / (float) registros.size()) * 100;
+	    System.out.println("Percentual de homens: " + percentualHomens + "%");
+	    
+	    float percentualMulheres = ((float) qtdMulheres / (float) registros.size()) * 100;
+	    System.out.println("Percentual de mulheres: " + percentualMulheres + "%");
+	}
+
+}
